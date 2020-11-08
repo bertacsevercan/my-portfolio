f873e3c253fa4413eaa8fbd2e9739f84ffb9fbce
bertacsevercan <bertac.severcan@gmail.com>
2020-11-08 12:49:31 +0300

    add necessary pages

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import {
   Route,
 } from "react-router-dom";
 import Home from './containers/Home';
+import Resume from './containers/Resume';
+import Contact from './containers/Contact';
+import Projects from './containers/Projects';
+import About from './containers/About';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/" component={Home} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/about" component={About} />
       </Router>
     </div>
   );
---
+++ src/containers/Projects/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects = () => {
+    return(
+        <div>
+            Home here!
+        </div>
+    )
+
+}
+
+export default Projects;
