0c695627518b941c723fcb4086140e7d661b58a6
bertacsevercan <bertac.severcan@gmail.com>
2020-11-08 13:33:30 +0300

    center navlinks

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -6,16 +6,26 @@ import Nav from "react-bootstrap/Nav";
 const NavBar = () => {
 
     return(
-        <Navbar bg="primary" variant="dark" expand="lg">
+        <Navbar bg="danger" variant="dark" expand="lg">
             <Navbar.Brand as={Link} to="/">My-Porfolio</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
+                <Nav className="justify-content-center" style={{ flex: 1}}>
+                <Nav.Item>
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to="/about">About Me</Nav.Link>
-                <Nav.Link as={Link} to="/resume">My Resume</Nav.Link>
-                <Nav.Link as={Link} to="/projects">My Projects</Nav.Link>
-                <Nav.Link as={Link} to="/contact">Contact Me</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link as={Link} to="/about">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                </Nav.Item>
                 </Nav>              
             </Navbar.Collapse>
         </Navbar>
