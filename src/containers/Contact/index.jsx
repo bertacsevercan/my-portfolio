7cb22004d852482d7a308a6fb9789a0a27da895d
bertacsevercan <bertac.severcan@gmail.com>
2020-11-10 01:16:20 +0300

    add name attirbute to inputs

--- src/containers/Contact/index.jsx
+++ src/containers/Contact/index.jsx
@@ -24,25 +24,25 @@ const Contact = () => {
         <Form name="contact" method="POST" data-netlify="true">
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Your Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control name="email" type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
               Please enter a valid email.
             </Form.Text>
           </Form.Group>
           <Form.Group controlId="formInputName">
             <Form.Label>Your Full Name</Form.Label>
-            <Form.Control placeholder="Full Name" />
+            <Form.Control name="fullName" placeholder="Full Name" />
             <Form.Text className="text-muted">E.g: John Doe</Form.Text>
           </Form.Group>
 
           <Form.Group controlId="formInputSubject">
             <Form.Label>Subject</Form.Label>
-            <Form.Control placeholder="Subject" />
+            <Form.Control name="subject" placeholder="Subject" />
             <Form.Text className="text-muted">What is it about?</Form.Text>
           </Form.Group>
           <Form.Group controlId="form.ControlTextarea">
             <Form.Label>Your Message</Form.Label>
-            <Form.Control placeholder="Hello, Bertaç!" as="textarea" rows={3} />
+            <Form.Control name="message" placeholder="Hello, Bertaç!" as="textarea" rows={3} />
           </Form.Group>
 
           <Button variant="danger" type="submit" block>
