db6f4cbf8c8e27c525117481fee0e693a948de13
bertacsevercan <bertac.severcan@gmail.com>
2020-11-09 21:42:55 +0300

    add new project

--- src/containers/Projects/iframes/index.jsx
+++ src/containers/Projects/iframes/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 const projectList = [
-    <iframe src="https://my-inventory.netlify.app/" height="500" width="80%" title="M.Y-Inventory"></iframe>,
     <iframe src="https://bertacsevercan.github.io/Madlibz/" height="500" width="80%" title="Madlibz"></iframe>,
+    <iframe src="https://bertacsevercan.github.io/Movie-Project/" height="500" width="80%" title="Madlibz"></iframe>,
+    <iframe src="https://my-inventory.netlify.app/" height="500" width="80%" title="M.Y-Inventory"></iframe>,
+
 ]
 
 export default projectList;
--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -4,15 +4,17 @@ import projectList from "./iframes";
 
 const Projects = () => {
 
-  const titleList = ["M.Y-Inventory", "Madlibz"];
-  const textList = ["Description goes here!", "Description goes here!"];
-  const repoList = ["https://github.com/bertacsevercan/tina-zita.v2", "https://github.com/bertacsevercan/Madlibz"];
-
+  const titleList = ["Madlibs", "Movie-Project", "M.Y-Inventory",  ];
+  const textList = ["Description goes here!", "Description goes here!", "Description goes here!"];
+  const repoList = ["https://github.com/bertacsevercan/Madlibz", "https://github.com/bertacsevercan/Movie-Project" 
+  ,"https://github.com/bertacsevercan/tina-zita.v2", ];
+  const linkList =["https://bertacsevercan.github.io/Madlibz/", "https://bertacsevercan.github.io/Movie-Project/",
+ "https://my-inventory.netlify.app/"];
   const projectObjList = [];
 
   for(let i = 0; i < projectList.length; i++){
     projectObjList.push({website: projectList[i], title: titleList[i], text: textList[i], 
-    repo: repoList[i]})
+    repo: repoList[i], link: linkList[i]})
 }
 
   return (
@@ -20,8 +22,9 @@ const Projects = () => {
     {projectObjList.map(project =>(
       <div className="project-wrapper"> 
       <h1>{project.title}</h1>
-      <h5><a target="_blank" rel="noreferrer" title="repo link" href={project.repo}>Github repository link.</a></h5>
+      <h5><a target="_blank" rel="noreferrer" title="repo link" href={project.repo}>Github repository.</a></h5>
       {project.website}
+      <h5><a target="_blank" rel="noreferrer" title="website link" href={project.link}>Go to website in a seperate page.</a></h5>
       <p>{project.text}</p>
       </div>
     ))}
