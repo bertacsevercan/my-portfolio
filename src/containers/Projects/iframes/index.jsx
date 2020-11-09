14be902d3947f44cd283f19d2db6b2650cf40861
bertacsevercan <bertac.severcan@gmail.com>
2020-11-09 14:13:06 +0300

    add iframes

---
+++ src/containers/Projects/iframes/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const projectList = [
+    <iframe src="https://my-inventory.netlify.app/" height="500" width="80%" title="M.Y-Inventory"></iframe>,
+
+
+]
+
+export default projectList;
--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./style.css";
+import projectList from "./iframes";
 
 const Projects = () => {
-  return <div>Projects here!</div>;
+
+  const titleList = ["M.Y-Inventory",];
+  const textList = ["Description goes here!",];
+  const repoList = ["https://github.com/bertacsevercan/tina-zita.v2",];
+
+  const projectObjList = [];
+
+  for(let i = 0; i < projectList.length; i++){
+    projectObjList.push({website: projectList[i], title: titleList[i], text: textList[i], 
+    repo: repoList[i]})
+}
+
+  return (
+  <div className="projects">
+    {projectObjList.map(project =>(
+      <div className="project-wrapper"> 
+      <h1>{project.title}</h1>
+      <h5><a target="_blank" rel="noreferrer" title="repo link" href={project.repo}>Github repository link.</a></h5>
+      {project.website}
+      <p>{project.text}</p>
+      </div>
+    ))}
+  </div>
+  );
 };
 
 export default Projects;
