60ec848b1f12355fd47f5c9d2ef7be45b9238133
bertacsevercan <bertac.severcan@gmail.com>
2020-11-09 22:47:54 +0300

    add more text to madlibs

--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
   ];
 
   const textList = [
-    "This is the first project that I've worked on during the time of the Re: Coded Bootcamp. For the first time, I did pair programming and worked on a project with a partner (Hello Knar!) and it was my first ever vanilla JavaScript, HTML, CSS project. Also, it was the first time that I learned about and worked with regular expressions, i18next library, local storage. The style of the project was purely CSS. I didn't use any frameworks for styling. Overall, I had so much fun with this project and learned a lot too.",
+    "This is the first project that I've worked on during the time of the Re: Coded Bootcamp. The purpose of the website is to play mad libs. It also has Turkish and English language support. It can save your words by using local storage.  For the first time, I did pair programming and worked on a project with a partner (Hello Knar!) and it was my first ever vanilla JavaScript, HTML, CSS project. Also, it was the first time that I learned about and worked with regular expressions, i18next library, local storage. The style of the project was purely CSS. I didn't use any frameworks for styling. Overall, I had so much fun with this project and learned a lot too.",
     "Description goes here!",
     "Description goes here!",
     "Description goes here!",
