24b1d512a40e61db87ff4c984dc95602f6364832
bertacsevercan <bertac.severcan@gmail.com>
2020-12-04 18:20:16 +0300

    add collapse on click to menu item

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,69 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
-import { FaLanguage } from "react-icons/fa";
+import { MdLanguage } from "react-icons/md";
 
 const NavBar = () => {
   const { t } = useTranslation();
+  const [expanded, setExpanded] = useState(false);
 
   return (
-    <Navbar bg="danger" variant="dark" expand="lg">
+    <Navbar expanded={expanded} bg="danger" variant="dark" expand="lg">
       <Navbar.Brand as={Link} to="/">
         Bertaç Severcan
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle
+        onClick={() => setExpanded(expanded ? false : "expanded")}
+        aria-controls="basic-navbar-nav"
+      />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="justify-content-end" style={{ flex: 1 }}>
-          <NavDropdown
-            title={<FaLanguage style={{ transform: "scale(1.4)" }} />}
-            id="basic-nav-dropdown"
-          >
-            <NavDropdown.Item
-              onClick={() => {
-                i18next.changeLanguage("tr");
-              }}
-              href="#action/3.1"
-            >
-              {t("navbar.languages.tr")}
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item
-              onClick={() => {
-                i18next.changeLanguage("en");
-              }}
-              href="#action/3.2"
-            >
-              {t("navbar.languages.en")}
-            </NavDropdown.Item>
-          </NavDropdown>
           <Nav.Item>
-            <Nav.Link as={Link} to="/">
+            <Nav.Link onClick={() => setExpanded(false)} as={Link} to="/">
               {t("navbar.links.link0")}
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link as={Link} to="/about">
+            <Nav.Link onClick={() => setExpanded(false)} as={Link} to="/about">
               {t("navbar.links.link1")}
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link as={Link} to="/resume">
+            <Nav.Link onClick={() => setExpanded(false)} as={Link} to="/resume">
               {t("navbar.links.link2")}
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link as={Link} to="/projects">
+            <Nav.Link
+              onClick={() => setExpanded(false)}
+              as={Link}
+              to="/projects"
+            >
               {t("navbar.links.link3")}
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link as={Link} to="/contact">
+            <Nav.Link
+              onClick={() => setExpanded(false)}
+              as={Link}
+              to="/contact"
+            >
               {t("navbar.links.link4")}
             </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <NavDropdown
+              title={<MdLanguage style={{ transform: "scale(1.5)" }} />}
+              drop="left"
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item
+                onClick={() => {
+                  i18next.changeLanguage("tr");
+                  setExpanded(false);
+                }}
+                href="#action/3.1"
+              >
+                {t("navbar.languages.tr")}
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item
+                onClick={() => {
+                  i18next.changeLanguage("en");
+                  setExpanded(false);
+                }}
+                href="#action/3.2"
+              >
+                {t("navbar.languages.en")}
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
