29f2664935491aef7008b4d7793c25c902bdb8af
bertacsevercan <bertac.severcan@gmail.com>
2020-12-06 00:20:02 +0300

    add footer scrollTop btn and socials

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { AiTwotoneHeart } from "react-icons/ai";
+import {
+  AiTwotoneHeart,
+  AiOutlineArrowUp,
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
 import { useTranslation } from "react-i18next";
+import Button from "react-bootstrap/Button";
 import "./style.css";
 
 const Footer = () => {
   const { t } = useTranslation();
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <div className="footer">
@@ -13,6 +23,27 @@ const Footer = () => {
           {t("footer.text0")} {<AiTwotoneHeart style={{ color: "red" }} />}{" "}
           {t("footer.text1")}
         </span>
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/bertac-severcan/"
+          title={t("contact.texts.text2")}
+        >
+          {" "}
+          <AiFillLinkedin id="linkedin-icon" />{" "}
+        </a>{" "}
+        <a
+          href="https://github.com/bertacsevercan"
+          rel="noreferrer"
+          target="_blank"
+          title={t("contact.texts.text1")}
+        >
+          <AiFillGithub id="github-icon" />
+        </a>{" "}
+        <Button onClick={scrollTop} variant="outline-danger" size="sm">
+          {" "}
+          {<AiOutlineArrowUp />}{" "}
+        </Button>
       </div>
     </div>
   );
