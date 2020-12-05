c9ffdffcca961a938da6b073ba636a9efa77f641
bertacsevercan <bertac.severcan@gmail.com>
2020-12-05 23:38:55 +0300

    add footer content

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import About from "./containers/About";
 import NavBar from "./components/NavBar";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/contact" component={Contact} />
           <Route path="/projects" component={Projects} />
           <Route path="/about" component={About} />
+          <Footer />
         </Router>
       </div>
     </I18nextProvider>
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { AiTwotoneHeart } from "react-icons/ai";
+import { useTranslation } from "react-i18next";
+import "./style.css";
 
-const Footer = () => {
+const Footer = () => {
+  const { t } = useTranslation();
 
-    return(
-        <div className="footer">
-
-
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <div className="container">
+        <span>
+          {t("footer.text0")} {<AiTwotoneHeart style={{ color: "red" }} />}{" "}
+          {t("footer.text1")}
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
